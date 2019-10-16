3f07d65efff2ce64661390ab13c5f77c8e4b9fce
Fauzan Idal Fithri El-Ardhi <37044826+d4rabi@users.noreply.github.com>
2019-10-16 19:35:40 +0700

    add example

---
+++ example/src/tiket/Hello.java
@@ -0,0 +1,10 @@
+package tiket;
+
+import java.rmi.Remote; 
+import java.rmi.RemoteException; 
+import java.util.*;
+
+// Creating Remote interface for our application 
+public interface Hello extends Remote {  
+   public List<Student> getStudents() throws Exception;  
+}
